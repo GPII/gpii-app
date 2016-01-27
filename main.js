a538713d7af8ab5d04061818cb78e361cd8a4570
Steven Githens <swgithen@mtu.edu>
2016-01-26 18:13:31 -0800

    Changes to pretty up task tray icon, and add start/stop gpii menu to tray

--- main.js
+++ main.js
@@ -5,20 +5,29 @@ var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var Menu = require('menu');
 var Tray = require("tray");
+var os = require("os");
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-var appIcon = null;
+var trayIcon = null;
 
 var startLocalFlowManager = function() {
     var fluid = require("universal"),
         gpii = fluid.registerNamespace("gpii");
 
+    if (os.platform() === 'win32') {
+        var windows = require("gpii-windows/index.js");
+    }
+
     gpii.start();
+};
+
+var stopLocalFlowManager = function() {
+    var configs = fluid.queryIoCSelector(fluid.rootComponent, "kettle.config");
+    fluid.each(configs, function (config) {config.destroy();});
 };
 
 // Quit when all windows are closed.
@@ -30,20 +39,63 @@ app.on('window-all-closed', function() {
     }
 });
 
+var buildContextMenu = function(gpiiStarted) {
+    var menu = [];
+    if (gpiiStarted) {
+        menu.push(
+            {
+                label: "Stop GPII",
+                click: function() {
+                    stopLocalFlowManager();
+                    trayIcon.setContextMenu(buildContextMenu(false));
+                }
+            });
+    }
+    else {
+        menu.push(
+            {
+                label: "Start GPII",
+                click: function() {
+                    startLocalFlowManager();
+                    trayIcon.setContextMenu(buildContextMenu(true));
+                }
+            });
+    }
+    menu.push({
+        label: "Exit",
+        click: function() {
+            app.quit();
+        }
+    });
+    return Menu.buildFromTemplate(menu);
+};
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-    var iconPath = path.join(__dirname, 'web/static/gpii.png');
-    console.log("ICONPATH: " + iconPath);
-    appIcon = new Tray(iconPath); //path.join(__dirname, 'web/static/gpii.ico'));
-    var contextMenu = Menu.buildFromTemplate([
-        { label: "Wow GPII" }
-    ]);
-    appIcon.setToolTip("GPII Electron");
-    appIcon.setContextMenu(contextMenu);
+    if (process.platform === 'darwin') {
+        trayIcon = new Tray(path.join(__dirname, 'web/icons/gpii-icon.png'));
+    }
+    else {
+        trayIcon = new Tray(path.join(__dirname, 'web/icons/gpii.ico'));
+    }
+    trayIcon.setToolTip("GPII Electron");
+    trayIcon.setContextMenu(buildContextMenu(true));;
+    startLocalFlowManager();
+    // startPersonaDemo();
+});
 
+/*
+ *  Code below if for the Persona Demo Window. Destined for another file or
+ *  module in the project.
+ */
+
+// Global Window Reference
+var personaDemoWindow = null;
+
+var startPersonaDemo = function() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({
+    personaDemoWindow = new BrowserWindow({
         width: 800, height: 600,
         "web-preferences": {
             "web-security": false
@@ -51,18 +103,17 @@ app.on('ready', function() {
     });
 
     // and load the index.html of the app.
-    mainWindow.loadUrl('file://' + __dirname + '/web/index.html');
+    personaDemoWindow.loadUrl('file://' + __dirname + '/web/index.html');
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    personaDemoWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    personaDemoWindow.on('closed', function() {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null;
+        personaDemoWindow = null;
     });
 
-    startLocalFlowManager();
-});
+};
