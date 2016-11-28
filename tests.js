18bdec231353fe40a81fa0d786343382cd39b97f
Michelle D'Souza <michelled33@gmail.com>
2016-11-28 10:52:38 -0500

    GPII-1596: Starting to write tests.

--- tests.js
+++ tests.js
@@ -4,5 +4,6 @@
 var app = require("electron").app;
 
 app.on("ready", function () {
-    require("./node_modules/gpii-windows/tests/UnitTests.js");
+    //require("./node_modules/gpii-windows/tests/UnitTests.js");
+    require("./tests/testMain.js");
 });
