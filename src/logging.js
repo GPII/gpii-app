bc3e03d8c22c044b012e2419897dbbd2de0b56ff
Michelle D'Souza <michelled33@gmail.com>
2017-01-19 16:04:04 -0500

    GPII-1596: Setting the environment to node in the eslint file and cleaning up the package.json file.

--- src/logging.js
+++ src/logging.js
@@ -1,6 +1,18 @@
+/*!
+GPII Application Logging
+Copyright 2017 Raising the Floor - International
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+The research leading to these results has received funding from the European Union's
+Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
+You may obtain a copy of the License at
+https://github.com/GPII/universal/blob/master/LICENSE.txt
+*/
+"use strict";
+
 var fs = require("fs");
 var fluid = require("infusion");
-
 var gpii = fluid.registerNamespace("gpii");
 
 var settingsDirComponent = gpii.settingsDir();
