f31231b355c779dd7890adf6a7a6321480cff632
Kasper Markus <kasper2@markus.dk>
2017-01-25 11:31:16 +0100

    GPII-2091: Updated references to universal and infusion. More script fixing. Improved README

--- tests.js
+++ tests.js
@@ -4,5 +4,7 @@
 var app = require("electron").app;
 
 app.on("ready", function () {
+    require("./node_modules/gpii-windows/node_modules/universal/tests/all-tests.js");
     require("./node_modules/gpii-windows/tests/UnitTests.js");
 });
+ 
