63f48d6519f04562851ffc31b54037089ee78eff
Alejandro Leiva <gloob@litio.org>
2017-02-06 14:51:50 +0100

    GPII-2206 : Updating installer script dealing with a pair of bugs and install explicitly electron-packager globally.

--- provisioning/Installer.ps1
+++ provisioning/Installer.ps1
@@ -37,6 +37,11 @@ md $stagingWindowsDir
 
 $appDir = Join-Path $stagingWindowsDir "app"
 
+# Install electron-packager globally.
+# TODO: Define electron-packager invocation in npm scripts.
+# TODO: Delete --prune when got fixed this issue https://github.com/electron-userland/electron-packager/issues/495
+Invoke-Command $npm "install electron-packager -g" $projectDir
+
 # Npm install the application, this needs to be done for packaging.
 Invoke-Command $npm "install" $projectDir
 
@@ -44,10 +49,12 @@ $packagerMetadata = "--app-copyright=`"Raising the Floor - International Associa
 
 $packagerDir = Join-Path $installerDir "packager"
 md $packagerDir
-Invoke-Command "electron-packager.cmd" "$projectDir --platform=win32 --arch=ia32 --overwrite --out=$packagerDir $packagerMetadata"
+Invoke-Command $npm "prune --production"
+Invoke-Command "electron-packager.cmd" "$projectDir --platform=win32 --arch=ia32 --no-prune --overwrite --out=$packagerDir $packagerMetadata"
 
 # Copying the packaged GPII-App content to staging/.
-$packagedAppDir = (Join-Path $packagerDir "gpii-app-win32-x64")
+# TODO: Try to avoid using the electron-packager directory name hardcoding it.
+$packagedAppDir = (Join-Path $packagerDir "gpii-app-win32-ia32")
 Copy-Item "$packagedAppDir\*" $stagingWindowsDir -Recurse
 
 # We are exiting with as a successful value if robocopy error is less or equal to 3
@@ -59,7 +66,7 @@ Invoke-Command "robocopy" "..\node_modules\gpii-windows\listeners $(Join-Path $s
 Invoke-Environment "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat"
 $msbuild = Get-MSBuild "4.0"
 $listenersDir = Join-Path $stagingWindowsDir "listeners"
-Invoke-Command $msbuild "listeners.sln /nodeReuse:false /p:Configuration=Release /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
+Invoke-Command $msbuild "listeners.sln /nodeReuse:false /p:Platform=Win32 /p:Configuration=Release /p:FrameworkPathOverride=`"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1`"" $listenersDir
 
 md (Join-Path $installerDir "output")
 md (Join-Path $installerDir "temp")
