53eb45c93bdd5ac4a97d3227454de09065737716
Michelle D'Souza <michelled33@gmail.com>
2017-05-04 14:41:27 -0400

    GPII-2092: Putting back exiting the process once the tests have complete.

--- tests.js
+++ tests.js
@@ -7,16 +7,14 @@ var app = require("electron").app,
 
 require("gpii-windows/index.js");
 
-// require("./node_modules/universal/tests/all-tests.js");
-// require("./node_modules/gpii-windows/tests/UnitTests.js");
+require("./node_modules/universal/tests/all-tests.js");
+require("./node_modules/gpii-windows/tests/UnitTests.js");
 
 app.on("ready", function () {
-    // require("./tests/testApp.js");
+    require("./tests/testApp.js");
     require("./tests/DevelopmentTests.js");
 
-    // jqUnit.onAllTestsDone.addListener(function (results) {
-    //     fluid.log("A total of ", results.total, " tests run, with ", results.failed, " failed tests. Exiting process with status ", results.failed);
-
-    //     process.exit(results.failed); // exits with 0 if no tests failed
-    // });
+    jqUnit.onAllTestsDone.addListener(function (results) {
+        process.exit(results.failed); // exits with 0 if no tests failed
+    });
 });
