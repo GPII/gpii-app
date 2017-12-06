f8f523f66a680e49f2d108a2d536470549780c83
Danail Dimitrov <dandimitrov@asteasolutions.com>
2017-12-06 19:39:22 +0200

    GPII-2732: Simplified implementation of survey components
    Introduce `surveyTriggerManager` and `surveyConnector`

---
+++ src/main/surveys/surveyConnector.js
@@ -0,0 +1,141 @@
+/*!
+GPII Application
+Copyright 2016 Steven Githens
+Copyright 2016-2017 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+The research leading to these results has received funding from the European Union's
+Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
+You may obtain a copy of the License at
+https://github.com/GPII/universal/blob/master/LICENSE.txt
+*/
+"use strict";
+
+var fluid = require("infusion");
+var ws    = require("ws");
+
+var gpii = fluid.registerNamespace("gpii");
+
+
+/**
+ * Send/receive survey data to/from the survey server
+ * occurred
+ */
+fluid.defaults("gpii.app.surveyConnector", {
+    gradeNames: ["fluid.modelComponent"],
+
+    model: {
+        machineId: null,
+        userId: null
+    },
+
+    config: {
+        // {1} - survey server implementation
+        // TODO update when survey server is implemented
+        surveyServerUrl: null
+    },
+
+    members: {
+        // TODO update when survey server is implemented
+        socket: null // "@expand:gpii.app.surveyConnector.connect({that}.options.config)"
+    },
+
+    listeners: {
+        "onCreate.register": {
+            funcName: "gpii.app.surveyConnector.register",
+            args: ["{that}.socket", "{that}.events"]
+        }
+    },
+
+    events: {
+        /*
+         * A survey payload is received.
+         */
+        onSurveyRequired: null,
+        /*
+         * A list of survey triggers that are to be registered
+         */
+        onTriggersReceived: null
+    },
+
+    invokers: {
+        requestTriggers: {
+            funcName: "gpii.app.surveyConnector.requestTriggers",
+            args: [
+                "{that}.socket",
+                "{that}.model"
+            ]
+        },
+        notifyTriggerOccurred: {
+            funcName: "gpii.app.surveyConnector.notifyTriggerOccurred",
+            args: [
+                "{that}.socket",
+                "{arguments}.0",
+                // XXX {1}
+                "{that}"
+            ]
+        }
+    }
+});
+
+
+gpii.app.surveyConnector.connect = function (config) {
+    return new ws(config.surveyServerUrl); // eslint-disable-line new-cap
+};
+
+/**
+ * XXX {1} Placeholder
+ */
+gpii.app.surveyConnector.requestTriggers = function (socket, payload) {
+    // socket.send("keyedIn", payload); // send `machineId` & `userId`
+};
+
+/**
+ * XXX {1} Placeholder
+ */
+gpii.app.surveyConnector.register = function (socket, events) {
+    // socket.on("surveyPayload", events.onSurveyRequired.fire); // JSON.parse
+    // socket.on("surveyTriggers", events.onTriggersReceived.fire);
+
+
+    var triggerFixture = {
+        surveyTrigger: {
+            conditions: [
+                {
+                    "minutesSinceKeyIn": 5
+                }
+            ],
+            id: "id",
+            urlTriggerHandler: "URL of the survey server to handle the surveyTriggerEvent"
+        }
+    };
+    events.onTriggersReceived.fire(triggerFixture);
+};
+
+
+gpii.app.surveyConnector.notifyTriggerOccurred = function (socket, trigger, that) {
+    // XXX {1} Posible implementation
+    // socket.send("surveyTrigger", { triggerId: trigger.id });
+
+    console.log("Survey Trigger Occured: ", trigger);
+    // XXX mocked behaviour
+    var surveyRawPayloadFixture = {
+        survey: {
+            "url": "https://www.example.com/surveyA/?safeid=longcodeA",
+            "window": {
+                "height": 600,  //optional
+                "width": 800,   //optional
+                "userResizable": true,
+                "titleBar": {
+                    "title": "GPII Auto-Personalization Survey",
+                    // "icon": "icon asset", //use gear-cloud icon by default
+                    "closeButton": true,     //default
+                    "minimizeButton": false, //default
+                    "maximizeButton": false  //default
+                }
+            }
+        }
+    };
+    that.events.onSurveyRequired.fire(surveyRawPayloadFixture);
+};
---
+++ src/main/surveys/surveyTriggerManager.js
@@ -0,0 +1,77 @@
+/*!
+GPII Application
+Copyright 2016 Steven Githens
+Copyright 2016-2017 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+The research leading to these results has received funding from the European Union's
+Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
+You may obtain a copy of the License at
+https://github.com/GPII/universal/blob/master/LICENSE.txt
+*/
+"use strict";
+
+var fluid = require("infusion");
+
+var gpii = fluid.registerNamespace("gpii");
+
+
+fluid.defaults("gpii.app.surveyTriggersManager", {
+    gradeNames: ["fluid.modelComponent"],
+
+    model: {
+        activeTimer: null
+    },
+
+
+    events: {
+        onTriggerOccurred: null
+    },
+
+    invokers: {
+        registerTrigger: {
+            funcName: "gpii.app.surveyTriggersManager.registerTrigger",
+            args: ["{that}", "{arguments}.0"]
+        },
+        clearTriggers: {
+            funcName: "gpii.app.surveyTriggersManager.clearTrigger",
+            args: ["{that}"]
+        }
+    }
+});
+
+
+gpii.app.surveyTriggersManager.registerTrigger = function (that, triggerData) {
+    that.clearTimers();
+
+    if (!triggerData) {
+        return;
+    }
+
+    var conditions = triggerData.conditions;
+    if (!conditions.length !== 1) {
+        console.log("SurveyTriggerManager: Unsoported number of conditions: ", conditions.length);
+        return;
+    }
+
+    // XXX mock
+    var timer;
+    if (conditions[0].minutesSinceKeyIn) {
+        timer = setTimeout(
+            function () {
+                console.log("SurveyTriggerManager: KeyedIn Timer triggered!");
+
+                delete triggerData.conditions;
+                that.events.onTriggerOccurred.fire(triggerData);
+            },
+            conditions[0].minutesSinceKeyIn * 10000
+        );
+        that.applier.change("activeTimer", timer);
+    }
+};
+
+
+gpii.app.surveyTriggersManager.clearTriggers = function (that) {
+    clearTimeout(that.model.activeTimer);
+};
