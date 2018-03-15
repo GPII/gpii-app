45f7c1605eeebb85ab18389bcf2d15516575ee9e
Danail Dimitrov <dandimitrov@asteasolutions.com>
2018-03-15 14:45:19 +0200

    GPII-2894: Add files loading mechanism prototype

--- src/messageBundles/messageBundlesMerge.js
+++ src/messageBundles/messageBundlesMerge.js
@@ -1,48 +1,5 @@
 "use-strict";
 
-var messageBundles = {
-    "en": {
-        // TODO naming
-        "gpii_app_menu_open-psp": "Open PSP",
-        "gpii_app_menu_status_keyed_in": "Keyed in with %snapsetName",
-        "gpii_app_menu_status_not_keyed": "(No one keyed in)",
-        "gpii_app_menu_keyed_out_btn": "Key-out of GPII",
-
-        gpii_app_restartWarning_restartTitle: "Changes require restart",
-        gpii_app_restartWarning_osRestartText: "Windows needs to restart to apply your changes.",
-        gpii_app_restartWarning_restartText: "In order to be applied, some of the changes you made require the following applications to restart:",
-        gpii_app_restartWarning_restartQuestion: "What would you like to do?",
-
-        gpii_app_restartWarning_undo: "Cancel\n(Undo Changes)",
-        gpii_app_restartWarning_restartLater: "Close and\nRestart Later",
-        gpii_app_restartWarning_restartNow: "Restart Now"
-    },
-    "en_us": {
-        // TODO naming
-        "gpii_app_menu_open-psp": "Open the PSP"
-    },
-    "en_gb": {
-        // TODO naming
-        "gpii_app_menu_open-psp": "To the PSP"
-    },
-    "bg": {
-        // персонален панел за настройки
-        // "gpii_app_menu_open-psp": "Отвори ППН",
-        // "gpii_app_menu_status_not_keyed": "(Не сте влезли)",
-        // "gpii_app_menu_status_keyed_in": "Вписан с %snapsetName",
-        // "gpii_app_menu_keyed_out_btn": "Отписване от GPII",
-
-        gpii_app_restartWarning_restartTitle: "Промените изискват рестартиране.",
-        gpii_app_restartWarning_osRestartText: "Windows изисква да бъде рестартиран, за да се приложат настройките.",
-        gpii_app_restartWarning_restartText: "За да могат част от вашите настройки да бъдат приложени, следните приложения трябва да бъдат презаредени:",
-        gpii_app_restartWarning_restartQuestion: "Какво бихте желали да направите?",
-
-        gpii_app_restartWarning_undo: "Отказ\n(Отменете промените)",
-        gpii_app_restartWarning_restartLater: "Затваряне и\n рестартиране по-късно",
-        gpii_app_restartWarning_restartNow: "Рестартиране сега"
-    }
-};
-
 // fr_FR, en -> ["en", "fr", "fr_FR"]
 function getLocaleVersions(locale, defaultLocale) {
     var segments = locale.split("_");
@@ -58,7 +15,7 @@ function getLocaleVersions(locale, defaultLocale) {
     return versions;
 }
 
-function mergeMessageBundles(messageBundles, defaultLocale) {
+function enchanceMessageBundles(messageBundles, defaultLocale) {
     var result = {};
 
     Object.keys(messageBundles).forEach(function (locale) {
@@ -78,3 +35,100 @@ function mergeMessageBundles(messageBundles, defaultLocale) {
 }
 
 console.log(mergeMessageBundles(messageBundles, "en"));
+
+
+/*
+give index
+read all folders of `messageBundles`
+merge these files under same locale
+merge locales
+*/
+
+/*
+
+app
+  |__ messageBundles
+  |__ src
+     |__ renderer
+        |__ 
+
+
+
+
+ */
+
+// TODO extarct
+// TODO search dir recursivly
+//
+// gpii-app-messageBundle_en.json
+// gpii-app-messageBundle_fr.json
+// gpii-app-messageBundle_bg.json
+//
+// gpii-psp-restartDialog_en.json
+//
+function loadMessageBundles(bundlesDir, fileType, parser) {
+    var fs = require("fs");
+    var path = require("path");
+    // read every json file from that index down (support subdirs?)
+    var files = fs.readdirSync(bundlesDir);
+    var typeRegex = "\\." + fileType + "$";
+    // TODO on error
+    var loadedFiles = files
+        .filter(function (filename) {
+            return filename.match(typeRegex);
+        })
+        .reduce(function (acc, filename) {
+            // TODO error
+            acc.push({
+                messages: parser.parse(fs.readFileSync(path.join(bundlesDir, filename))),
+                filename: filename
+            });
+
+            return acc;
+        }, []);
+
+    return loadedFiles;
+};
+
+function extractLocaleFromFilename(filename) {
+    // strip file extension
+    filename = filename.split(".")[0];
+
+    var localeSegStart = filename.indexOf("_") + 1;
+    return filename.slice(localeSegStart);
+}
+
+/*
+ * Merge all different bundles under same locale.
+ *
+ Results:
+ {
+    en: {
+        val: message,
+        ...
+    },
+    bg: {
+        val: message,
+        ...
+    }
+ }
+ */
+function constructMessageBundles(loadedBundles) {
+    var messageBundles = {};
+
+    loadedBundles.forEach(function (bundle) {
+        var locale = extractLocaleFromFilename(bundle.filename),
+            prevMessages = messageBundles[locale] || {};
+
+        // merge with the new file's messages
+        messageBundles[locale] = Object.assign(prevMessages, bundle.messages);
+    });
+
+    return messageBundles;
+};
+
+/*
+var messageBundlesList = loadMessageBundles("./src/messageBundles", "json", JSON);
+var rawMessageBundles = constructMessageBundles(messageBundlesList);
+var compiledMessageBundle = enchanceMessageBundles(rawMessageBundles, "en");
+*/
