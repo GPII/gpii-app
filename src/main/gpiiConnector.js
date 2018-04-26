012ed6623a56b467257ff9d00958bae12d099432
Georgi Todorov <georgi.todorov@asteasolutions.com>
2018-04-26 14:03:42 +0300

    GPII-2940: Adding a default group of settings

--- src/main/gpiiConnector.js
+++ src/main/gpiiConnector.js
@@ -32,7 +32,15 @@ fluid.defaults("gpii.app.gpiiConnector", {
         onSnapsetNameUpdated: null,
         groupSettings: {
             event: "onMessageReceived",
-            args: "@expand:gpii.app.gpiiConnector.groupSettings({arguments}.0)"
+            args: {
+                expander: {
+                    funcName: "gpii.app.gpiiConnector.groupSettings",
+                    args: [
+                        groupingTemplate,
+                        "{arguments}.0" // message
+                    ]
+                }
+            }
         }
     },
 
@@ -92,10 +100,15 @@ gpii.app.gpiiConnector.getSettingControls = function (element, groupSolutionName
             if (setting.settings) {
                 var subsettingControls =
                     gpii.app.gpiiConnector.getSettingControls(setting, groupSolutionName, messageSettingControls);
-                if (fluid.keys(subsettingControls).length > 0) {
+                if (gpii.app.isHashNotEmpty(subsettingControls)) {
                     settingControls[path].settingControls = subsettingControls;
                 }
             }
+
+            // Add a flag to the original setting descriptor to indicate that the setting
+            // has been assigned to a group. Needed to determine which settings should go
+            // into the default group.
+            settingDescriptor.grouped = true;
         }
     });
 
@@ -103,13 +116,69 @@ gpii.app.gpiiConnector.getSettingControls = function (element, groupSolutionName
 };
 
 /**
+ *Given the `settingControls` object which was part of the message received
+ * via the PSP channel, this function creates setting groups based on the
+ * `groupingTemplate`. Each object in the resulting array represents a single
+ * group that is to be visualized in the PSP and will definitely have at least
+ * one setting present.
+ * @param groupingTemplate {Array} A template array which serves for constructing
+ * setting groups.
+ * @result {Object[]} An array of setting groups each of which containing at
+ * least one setting.
+ */
+gpii.app.gpiiConnector.createGroupsFromTemplate = function (groupingTemplate, messageSettingControls) {
+    return fluid.copy(groupingTemplate)
+        .map(function (templateGroup) {
+            return {
+                name: templateGroup.name,
+                solutionName: templateGroup.solutionName,
+                settingControls: gpii.app.gpiiConnector.getSettingControls(templateGroup, templateGroup.solutionName, messageSettingControls)
+            };
+        })
+        .filter(function (settingGroup) {
+            return gpii.app.isHashNotEmpty(settingGroup.settingControls);
+        });
+};
+
+/**
+ *Given the `settingControls` object which was part of the message received
+ * via the PSP channel, this function creates a default group of settings, i.e.
+ * it will contain all settings which have not been assigned to other groups
+ * based on the `groupingTemplate`.
+ * @param messageSettingControls {Object} The `settingControls` object from the
+ * PSP channel message.
+ * @result {Object} The default group or `undefined` if there are no settings
+ * which do not belong to other groups.
+ */
+gpii.app.gpiiConnector.createDefaultGroup = function (messageSettingControls) {
+    var defaultGroup = {
+        settingControls: {}
+    };
+
+    fluid.each(messageSettingControls, function (settingDescriptor, path) {
+        if (!settingDescriptor.grouped) {
+            defaultGroup.settingControls[path] = fluid.copy(settingDescriptor);
+        }
+    });
+
+    if (gpii.app.isHashNotEmpty(defaultGroup.settingControls)) {
+        return defaultGroup;
+    }
+};
+
+/**
  * Transforms the message received via the PSP channel by adapting it
  * to the format expected by the PSP. The main thing that this function
  * does is to create groups of settings. Each group has a name and a set
  * of settings each of which can also have settings.
+ * @param groupingTemplate {Array} A template array which serves for constructing
+ * setting groups. Each group within the template must have a `settings` element
+ * which specifies the paths and possibly the subsettings of these settings, and
+ * optionally a `name` which will be displayed as a title for the group and a
+ * `solutionName` which will be used in the restart warning text.
  * @param message {Object} The received message.
  */
-gpii.app.gpiiConnector.groupSettings = function (message) {
+gpii.app.gpiiConnector.groupSettings = function (groupingTemplate, message) {
     var payload = message.payload || {},
         operation = payload.type,
         path = payload.path,
@@ -117,20 +186,21 @@ gpii.app.gpiiConnector.groupSettings = function (message) {
         messageSettingControls = value.settingControls;
 
     if (operation === "ADD" && path.length === 0 && messageSettingControls) {
-        value.settingGroups = fluid.copy(groupingTemplate)
-            .map(function (templateGroup) {
-                return {
-                    name: templateGroup.name,
-                    solutionName: templateGroup.solutionName,
-                    settingControls: gpii.app.gpiiConnector.getSettingControls(templateGroup, templateGroup.solutionName, messageSettingControls)
-                };
-            })
-            .filter(function (settingGroup) {
-                return fluid.keys(settingGroup.settingControls).length > 0;
-            });
+        // First, add the groups which can be constructed from the grouping template
+        value.settingGroups = gpii.app.gpiiConnector.createGroupsFromTemplate(groupingTemplate, messageSettingControls);
 
+        // Then create a default group with all settings that have not yet been assigned
+        // to other groups.
+        var defaultGroup = gpii.app.gpiiConnector.createDefaultGroup(messageSettingControls);
+        if (defaultGroup) {
+            value.settingGroups.unshift(defaultGroup);
+        }
+
+        // Remove the `settingControls` element as it is no longer needed.
         delete value.settingControls;
     }
+
+    return message;
 };
 
 /**
