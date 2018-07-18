aa7dc6676de04f4b3772635c3bf390309c2d73a5
Georgi Todorov <georgi.todorov@asteasolutions.com>
2018-07-18 17:19:22 +0300

    GPII-3183: Tests for the QSS tooltip

--- tests/QssTestDefs.js
+++ tests/QssTestDefs.js
@@ -20,7 +20,10 @@ var fluid = require("infusion"),
     jqUnit = fluid.require("node-jqunit", require, "jqUnit"),
     gpii = fluid.registerNamespace("gpii");
 
-var clickCloseBtn = "document.querySelector(\".flc-quickSetStrip > div:last-child\").click()";
+var clickCloseBtn = "jQuery(\".flc-quickSetStrip > div:last-child\").click()",
+    hoverCloseBtn = "jQuery(\".flc-quickSetStrip > div:last-child\").trigger(\"mouseenter\")",
+    unhoverCloseBtn = "jQuery(\".flc-quickSetStrip > div:last-child\").trigger(\"mouseleave\")",
+    focusCloseBtn = "var event = jQuery.Event(\"keyup\"); event.shiftKey = true; event.key = \"Tab\"; jQuery(\".flc-quickSetStrip > div:first-child\").trigger(event)";
 
 require("../src/main/app.js");
 
@@ -40,6 +43,31 @@ gpii.tests.qss.executeCommand = function (dialog, command) {
     dialog.webContents.executeJavaScript(command, true);
 };
 
+gpii.tests.qss.testPspAndQssVisibility = function (app, params) {
+    jqUnit.assertEquals(
+        "PSP has correct visibility state",
+        params.psp,
+        app.psp.model.isShown
+    );
+
+    jqUnit.assertEquals(
+        "QSS has correct visibility state",
+        params.qss,
+        app.qssWrapper.qss.model.isShown
+    );
+};
+
+// XXX: For dev purposes.
+gpii.tests.qss.linger = function () {
+    var promise = fluid.promise();
+
+    setTimeout(function () {
+        promise.resolve();
+    }, 2000);
+
+    return promise;
+};
+
 gpii.tests.qss.testDefs = {
     name: "QSS Widget integration tests",
     // expect: 1,
@@ -48,37 +76,87 @@ gpii.tests.qss.testDefs = {
         configPath: "tests/configs"
     },
     gradeNames: ["gpii.test.common.testCaseHolder"],
-    sequence: [{
+    sequence: [{ // Wait for the QSS to initialize.
         task: "gpii.tests.qss.awaitQssInitialization",
         args: ["{that}.app.qssWrapper.qss"],
         resolve: "jqUnit.assert",
         resolveArgs: ["QSS has initialized successfully"]
-    }, {
-        func: "jqUnit.assertFalse",
+    }, { // At first, neither the PSP, nor the QSS is shown.
+        func: "gpii.tests.qss.testPspAndQssVisibility",
         args: [
-            "QSS is not shown when the application is started",
-            "{that}.app.qssWrapper.qss.model.isShown"
+            "{that}.app",
+            {psp: false, qss: false}
         ]
-    }, {
+    }, { // When the tray icon is clicked...
         func: "{that}.app.tray.events.onTrayIconClicked.fire"
-    }, {
-        func: "jqUnit.assertTrue",
+    }, { // ... both the PSP and the QSS will be shown.
+        func: "gpii.tests.qss.testPspAndQssVisibility",
         args: [
-            "QSS is shown when the tray icon is clicked",
-            "{that}.app.qssWrapper.qss.model.isShown"
+            "{that}.app",
+            {psp: true, qss: true}
         ]
-    }, {
+    }, { // Clicking on the close button in the QSS...
         func: "gpii.tests.qss.executeCommand",
         args: [
             "{that}.app.qssWrapper.qss.dialog",
             clickCloseBtn
         ]
-    }, {
+    }, { // ... results in both the PSP and the QSS being hidden.
         event: "{that}.app.qssWrapper.qss.channelListener.events.onQssClosed",
+        listener: "gpii.tests.qss.testPspAndQssVisibility",
+        args: [
+            "{that}.app",
+            {psp: false, qss: false}
+        ]
+    }, { // Open the QSS...
+        func: "{that}.app.tray.events.onTrayIconClicked.fire"
+    }, {
+        func: "jqUnit.assertFalse",
+        args: [
+            "The QSS tooltip is not shown initially",
+            "{that}.app.qssWrapper.qssTooltip.model.isShown"
+        ]
+    }, { // ... and hover on its close button.
+        func: "gpii.tests.qss.executeCommand",
+        args: [
+            "{that}.app.qssWrapper.qss.dialog",
+            hoverCloseBtn
+        ]
+    }, { // This will bring up the tooltip for that button.
+        changeEvent: "{that}.app.qssWrapper.qssTooltip.applier.modelChanged",
+        path: "isShown",
+        listener: "jqUnit.assertTrue",
+        args: [
+            "The QSS tooltip is shown when a button is hovered",
+            "{that}.app.qssWrapper.qssTooltip.model.isShown"
+        ]
+    }, { // When the button is no longer hovered...
+        func: "gpii.tests.qss.executeCommand",
+        args: [
+            "{that}.app.qssWrapper.qss.dialog",
+            unhoverCloseBtn
+        ]
+    }, { // ... the tooltip is gone.
+        changeEvent: "{that}.app.qssWrapper.qssTooltip.applier.modelChanged",
+        path: "isShown",
         listener: "jqUnit.assertFalse",
         args: [
-            "QSS is closed when its close button is clicked",
-            "{that}.app.qssWrapper.qss.model.isShown"
+            "The QSS tooltip is hidden when the button is no longer hovered",
+            "{that}.app.qssWrapper.qssTooltip.model.isShown"
+        ]
+    }, { // If the button is focused using keyboard interaction...
+        func: "gpii.tests.qss.executeCommand",
+        args: [
+            "{that}.app.qssWrapper.qss.dialog",
+            focusCloseBtn
+        ]
+    }, {
+        changeEvent: "{that}.app.qssWrapper.qssTooltip.applier.modelChanged",
+        path: "isShown",
+        listener: "jqUnit.assertTrue",
+        args: [
+            "The QSS tooltip is shown when a button is focused using the keyboard",
+            "{that}.app.qssWrapper.qssTooltip.model.isShown"
         ]
     }]
 };
