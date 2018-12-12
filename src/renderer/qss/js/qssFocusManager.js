f97349978920a322dbef4cf2e9fd03ca681c243f
Georgi Todorov <georgi.todorov@asteasolutions.com>
2018-12-12 16:15:17 +0200

    GPII-3573: Basics for arrow navigation in the QSS

--- src/renderer/qss/js/qssFocusManager.js
+++ src/renderer/qss/js/qssFocusManager.js
@@ -26,24 +26,40 @@
      */
     fluid.defaults("gpii.qss.qssFocusManager", {
         gradeNames: ["gpii.qss.horizontalFocusManager", "gpii.qss.verticalFocusManager"],
+
+        maxElementsPerFocusGroup: 2,
+
+        styles: {
+            smallButton: "fl-qss-smallButton"
+        },
+
         listeners: {
             "onArrowUpPressed.impl": {
-                func: "fluid.identity"
+                funcName: "gpii.qss.qssFocusManager.onArrowUpPressed",
+                args: ["{that}"]
             },
             "onArrowDownPressed.impl": {
-                func: "fluid.identity"
+                funcName: "gpii.qss.qssFocusManager.onArrowDownPressed",
+                args: ["{that}"]
             },
             "onArrowLeftPressed.impl": {
-                func: "fluid.identity"
+                funcName: "gpii.qss.qssFocusManager.onArrowLeftPressed",
+                args: ["{that}"]
             },
             "onArrowRightPressed.impl": {
-                func: "fluid.identity"
+                funcName: "gpii.qss.qssFocusManager.onArrowRightPressed",
+                args: ["{that}"]
             }
         },
+
         invokers: {
             getFocusInfo: {
                 funcName: "gpii.qss.qssFocusManager.getFocusInfo",
                 args: ["{that}.container", "{that}.options.styles"]
+            },
+            getFocusGroupsInfo: {
+                funcName: "gpii.qss.qssFocusManager.getFocusGroupsInfo",
+                args: ["{that}", "{that}.container"]
             }
         }
     });
@@ -97,5 +113,140 @@
             focusableElements: focusableElements,
             focusIndex: focusIndex
         };
+    };
+
+    /**
+     * Returns an array of focus groups each of which can contain a different number of QSS
+     * buttons (DOM elements). The rules for grouping QSS buttons is as follows:
+     * 1. The buttons are examined in the order in which they appear in the DOM.
+     * 2. Each group can have no more than `maxElementsPerFocusGroup` elements.
+     * 3. A group can have multiple buttons if and only if they are "small" QSS buttons.
+     * 4. A group can contain only focusable elements.
+     * @param {Component} that - The `gpii.qss.qssFocusManager` instance.
+     * @param {jQuery} container - The jQuery element representing the container of the component.
+     * @param {Object[]} An array of arrays the latter of which contain DOM elements representing
+     * the QSS buttons.
+     */
+    gpii.qss.qssFocusManager.getFocusGroups = function (that, container) {
+        var focusGroups = [],
+            styles = that.options.styles,
+            focusableElements = container.find("." + styles.focusable + ":visible"),
+            currentFocusGroup = [];
+
+        fluid.each(focusableElements, function (focusableElement) {
+            if (jQuery(focusableElement).hasClass(styles.smallButton)) {
+                currentFocusGroup.push(focusableElement);
+                // Mark the group as complete if the max number of elements has been reached.
+                if (currentFocusGroup.length >= that.options.maxElementsPerFocusGroup) {
+                    focusGroups.push(currentFocusGroup);
+                    currentFocusGroup = [];
+                }
+            } else {
+                // A "big" button can only be added to a group where it is the only element.
+                // So complete any partial group that may exist before that.
+                if (currentFocusGroup.length > 0) {
+                    focusGroups.push(currentFocusGroup);
+                    currentFocusGroup = [];
+                }
+                focusGroups.push([focusableElement]);
+            }
+        });
+
+        return focusGroups;
+    };
+
+    /**
+     * Returns information about the available focusable elements in the QSS, the index of the
+     * focus group in which the currently focused element resides and the index of the focused
+     * element within its own group.
+     * @param {Component} that - The `gpii.qss.qssFocusManager` instance.
+     * @param {jQuery} container - The jQuery element representing the container of the component.
+     * @return {Object} - An object containing information about the focusable elements in the QSS.
+     */
+    gpii.qss.qssFocusManager.getFocusGroupsInfo = function (that, container) {
+        var focusGroups = gpii.qss.qssFocusManager.getFocusGroups(that, container),
+            focusedElement = container.find("." + that.options.styles.focused)[0];
+
+        var focusIndex = -1,
+            focusGroupIndex = fluid.find(focusGroups, function (focusGroup, index) {
+                focusIndex = jQuery.inArray(focusedElement, focusGroup);
+                if (focusIndex > -1) {
+                    return index;
+                }
+            });
+
+        return {
+            focusGroups: focusGroups,
+            focusGroupIndex: focusGroupIndex,
+            focusIndex: focusIndex
+        };
+    };
+
+    gpii.qss.qssFocusManager.onArrowUpPressed = function (that) {
+        var focusGroupInfo = that.getFocusGroupsInfo(),
+            focusGroupIndex = focusGroupInfo.focusGroupIndex,
+            previousElementIndex = focusGroupInfo.focusIndex - 1,
+            focusGroups = focusGroupInfo.focusGroups,
+            focusGroup = focusGroups[focusGroupIndex];
+
+        // If there is a focused group at all and the currently focused element is not
+        // the first one in the focus group
+        if (focusGroupIndex > -1 && previousElementIndex >= 0) {
+            var elementToFocus = focusGroup[previousElementIndex];
+            that.focusElement(jQuery(elementToFocus), true);
+        }
+    };
+
+    gpii.qss.qssFocusManager.onArrowDownPressed = function (that) {
+        var focusGroupInfo = that.getFocusGroupsInfo(),
+            focusGroupIndex = focusGroupInfo.focusGroupIndex,
+            nextElementIndex = focusGroupInfo.focusIndex + 1,
+            focusGroups = focusGroupInfo.focusGroups,
+            focusGroup = focusGroups[focusGroupIndex];
+
+        // If there is a focused group at all and the currently focused element is not
+        // the first one in the focus group
+        if (focusGroupIndex > -1 && nextElementIndex < focusGroup.length) {
+            var elementToFocus = focusGroup[nextElementIndex];
+            that.focusElement(jQuery(elementToFocus), true);
+        }
+    };
+
+    gpii.qss.qssFocusManager.onArrowLeftPressed = function (that) {
+        var focusGroupInfo = that.getFocusGroupsInfo(),
+            focusGroupIndex = focusGroupInfo.focusGroupIndex,
+            focusGroups = focusGroupInfo.focusGroups,
+            focusIndex = focusGroupInfo.focusIndex,
+            previousGroupIndex;
+
+        if (focusGroupIndex < 0) {
+            previousGroupIndex = focusGroups.length - 1;
+        } else {
+            previousGroupIndex = gpii.psp.modulo(focusGroupIndex - 1, focusGroups.length);
+        }
+
+        var previousFocusGroup = focusGroups[previousGroupIndex],
+            elementIndex = Math.min(focusIndex, previousFocusGroup.length - 1),
+            elementToFocus = previousFocusGroup[elementIndex];
+        that.focusElement(jQuery(elementToFocus), true);
+    };
+
+    gpii.qss.qssFocusManager.onArrowRightPressed = function (that) {
+        var focusGroupInfo = that.getFocusGroupsInfo(),
+            focusGroupIndex = focusGroupInfo.focusGroupIndex,
+            focusGroups = focusGroupInfo.focusGroups,
+            focusIndex = focusGroupInfo.focusIndex,
+            nextGroupIndex;
+
+        if (focusGroupIndex < 0) {
+            nextGroupIndex = 0;
+        } else {
+            nextGroupIndex = gpii.psp.modulo(focusGroupIndex + 1, focusGroups.length);
+        }
+
+        var previousFocusGroup = focusGroups[nextGroupIndex],
+            elementIndex = Math.min(focusIndex, previousFocusGroup.length - 1),
+            elementToFocus = previousFocusGroup[elementIndex];
+        that.focusElement(jQuery(elementToFocus), true);
     };
 })(fluid, jQuery);
