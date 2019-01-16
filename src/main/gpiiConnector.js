7c2f4672c8eb988d19669dc1b7457256cfaa1818
Danail Dimitrov <dandimitrov@asteasolutions.com>
2019-01-16 11:51:49 +0200

    GPII-3608: Set proper values for both Scale factor and language preferences

--- src/main/gpiiConnector.js
+++ src/main/gpiiConnector.js
@@ -322,17 +322,37 @@ gpii.app.extractSnapsetName = function (message) {
     return preferences.name;
 };
 
+
 /**
- * Extension of `gpiiController` used for dev purposes.
+ * Extension of `gpiiController` used for dev purposes. Applies changes to specific settings.
  */
 fluid.defaults("gpii.app.dev.gpiiConnector", {
     gradeNames: ["gpii.app.gpiiConnector", "gpii.app.dev.gpiiConnector.qss"],
 
+    // Options for settings that need adjusting
+    tweakedSettingOptions: {
+        qssSettingMessagesPrefix: "gpii_app_qss_settings",
+        paths: {
+            screenScale: "http://registry\\.gpii\\.net/common/DPIScale",
+            language:    "http://registry\\.gpii\\.net/common/language"
+        }
+    },
+
     events: {
         // Decorate events' arguments
-        mockPrefSets: {
+        tweakPrefSets: {
             event: "onPreferencesUpdated",
-            args: "@expand:gpii.app.dev.gpiiConnector.mockPreferences({arguments}.0)"
+            args: {
+                expander: {
+                    func: "gpii.app.dev.gpiiConnector.decoratePreferences",
+                    args: [
+                        "{app}.systemLanguageListener",
+                        "{messageBundles}.model.messages",
+                        "{that}.options.tweakedSettingOptions",
+                        "{arguments}.0"
+                    ]
+                }
+            }
         },
         groupSettings: {
             event: "onMessageReceived",
@@ -387,17 +407,48 @@ gpii.app.dev.gpiiConnector.applyPrefSetImages = function (prefSets) {
     });
 };
 
-/*
- * A decorator for the extracted preferences that applies values that are to be used
- * for development purposes.
- * @param {Preferences} preferences - The preferences that are
- * to be decorated
+
+
+/**
+ * Updates the schemas of specific settings.
+ * This is a temporary measure and settings should be provided properly by the gpii-universal.
+ * @param {Component} systemLanguageListener - The `gpii.windows.language` instance
+ * @param {Object} qssSettingMessages - The messages bundle. This is needed to provide correct (and synced with the QS)
+ * names for different settings
+ * @param {Object} tweakedSettingOptions - Useful options for the tweaked settings
+ * @param {Object[]} settings - The settings that are to be decorated
  */
-gpii.app.dev.gpiiConnector.mockPreferences = function (preferences) {
+gpii.app.dev.gpiiConnector.tweakSettingSchemas = function (systemLanguageListener, qssSettingMessages, tweakedSettingOptions, settings) {
+    fluid.each(settings, function (setting) {
+        if (setting.path === tweakedSettingOptions.paths.language) {
+            setting.schema["enum"] = fluid.keys(systemLanguageListener.model.installedLanguages);
+        } else if (setting.path === tweakedSettingOptions.paths.screenScale) {
+            setting.schema.min = gpii.windows.display.getScreenDpi().minimum;
+            setting.schema.max = gpii.windows.display.getScreenDpi().maximum;
+            // Get the proper title for the setting.
+            // Note that this would also be affected by i18n, once we have translations for the QS settings
+            setting.schema.title = qssSettingMessages["common-DPIScale"].title;
+        }
+    });
+};
+
+/**
+ * A decorator for the received preferences which applies specific property tweaks where needed.
+ * This it to be used as a temporary measure until corresponding functionality is introduced in the gpii-universal.
+ * @param {Component} systemLanguageListener - The `gpii.windows.language` instance
+ * @param {Object} messages - The messages bundle. This is needed to provide correct (and synced with the QS)
+ * names for different settings
+ * @param {Object} tweakedSettingOptions - Useful options for the tweaked settings
+ * @param {Preferences} preferences - The preferences that are to be decorated
+ */
+gpii.app.dev.gpiiConnector.decoratePreferences = function (systemLanguageListener, messages, tweakedSettingOptions, preferences) {
     gpii.app.dev.gpiiConnector.applyPrefSetImages(preferences.sets);
 
+    // make some tweaks...
     fluid.each(preferences.settingGroups, function (settingGroup) {
         gpii.app.dev.gpiiConnector.applyLivenessFlag(settingGroup.settings);
+        gpii.app.dev.gpiiConnector.tweakSettingSchemas(
+            systemLanguageListener, messages[tweakedSettingOptions.qssSettingMessagesPrefix], tweakedSettingOptions, settingGroup.settings);
     });
 };
 
