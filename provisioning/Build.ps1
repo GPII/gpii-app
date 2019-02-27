dd521bfec4156eb64bad6ff162de121420978cb7
Javier Hernández <javi@raisingthefloor.org>
2019-02-27 11:16:09 +0100

    GPII-3744: Updated Build.ps1 to also install CouchDB

--- provisioning/Build.ps1
+++ provisioning/Build.ps1
@@ -30,7 +30,7 @@ Import-Module $bootstrapModule -Verbose -Force
 # # Run all the windows provisioning scripts
 # ############
 # TODO: Create function for downloading scripts and executing them.
-$windowsBootstrapURL = "https://raw.githubusercontent.com/GPII/windows/master/provisioning"
+$windowsBootstrapURL = "https://raw.githubusercontent.com/javihernandez/windows/GPII-3744/provisioning"
 try {
     $choco = Join-Path $originalBuildScriptPath "Chocolatey.ps1"
     Write-OutPut "Running windows script: $choco"
@@ -38,6 +38,15 @@ try {
     Invoke-Expression $choco
 } catch {
     Write-OutPut "Chocolatey.ps1 FAILED"
+    exit 1
+}
+try {
+    $couchdb = Join-Path $originalBuildScriptPath "CouchDB.ps1"
+    Write-OutPut "Running windows script: $couchdb"
+    iwr "$windowsBootstrapURL/CouchDB.ps1" -UseBasicParsing -OutFile $couchdb
+    Invoke-Expression $couchdb
+} catch {
+    Write-OutPut "CouchDB.ps1 FAILED"
     exit 1
 }
 try {
