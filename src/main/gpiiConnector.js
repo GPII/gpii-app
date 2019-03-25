4070f9a06767b2e978f4663821435272a53e5184
Yanachkov <kris.yanachkov@gmail.com>
2019-03-25 15:27:47 +0200

    GPII-3819: code review changes

--- src/main/gpiiConnector.js
+++ src/main/gpiiConnector.js
@@ -770,6 +770,7 @@ fluid.defaults("gpii.app.dev.gpiiConnector.qss", {
         "http://registry\\.gpii\\.net/common/highContrastTheme": { value: "regular-contrast" },
         "http://registry\\.gpii\\.net/common/selfVoicing/enabled": { value: false },
         // use the initial value of the language as default setting
+        // This is temporary solution related to GPII-3819 issue. After finding permanent solution the value should be taken from the core.
         "http://registry\\.gpii\\.net/common/language": { value: "{that}.options.defaultPreferences.systemDefaultLanguage" }
     }
 });
